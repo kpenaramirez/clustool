5c4be6a39ee91484359f470dc78fc62f7450767a
Jorge Anais <jrganais@gmail.com>
2022-09-02 00:45:59 -0400

    Running button working properly (dirty trick)

--- src/components/run_button.py
+++ src/components/run_button.py
@@ -12,14 +12,28 @@ from ..processing import PREPROCESSORS, CLUSTERERS
 def render(app: Dash, source: DataSource) -> html.Div:
 
     @app.callback(
+        Output(ids.RUN_BUTTON, 'disabled'),
+        Output(ids.RUN_BUTTON, 'children'),
+        Output(ids.RUN_BUTTON, 'className'),
+        Input(ids.RUN_BUTTON, "n_clicks"),
+    )
+    def hide_newbutton(n_clicks: int) -> bool:
+        if n_clicks is None:
+            return False, "RUN", "btn btn-primary"
+        else:
+            return True, "Running", "btn btn-light"
+
+    @app.callback(
         Output(ids.RESPONSE_TEXT_BUTTON, "children"),
         Output(ids.RESPONSE_TEXT_BUTTON, "className"),
+        Output('dynamic-button-container', 'children'),
         Input(ids.RUN_BUTTON, "n_clicks"),
         State(ids.COLUMNS_SELECTION_DROPDOWN, "value"),
         State(ids.PREPROC_DROPDOWN, "value"),
         State(ids.PREPROC_PARAMS_DIV, "children"),
         State(ids.CLUSTERING_DROPDOWN, "value"),
         State(ids.CLUSTERING_PARAMS_DIV, "children"),
+        State('dynamic-button-container', 'children'),
     )
     def press_button(
         n_clicks: int,
@@ -28,13 +42,26 @@ def render(app: Dash, source: DataSource) -> html.Div:
         preproc_div: dict,
         clustering_selection: str,
         clustering_div: dict,
-    ) -> tuple[str, str]:
+        children: html.Div,
+    ) -> tuple[str, str, html.Div]:
         """Run the preprocessing and clustering using the definded functions and respective parameters"""
 
-
+        
         pipeline = []
 
+        # Trick to disable the button
+        new_button = html.Button(
+            "Run",
+            id = ids.RUN_BUTTON, 
+            className="btn btn-primary",
+            style={"margin-right": "10px", "width": "100px"},
+		)
+
+        children.pop()
+        children.append(new_button)
+
         if n_clicks:
+            
             if columns and clustering_selection:
 
                 # Preprocessing
@@ -64,34 +91,40 @@ def render(app: Dash, source: DataSource) -> html.Div:
                 )
 
                 if any(p is None for p in clustering_params_values):
-                    return "Please fill all the parameters", "alert alert-warning"
+                    return "Please fill all the parameters", "alert alert-warning", children
 
                 pipeline.append(partial(clustering_function, **clustering_params))
 
                 # Run the pipeline
                 try:
                     source.process(columns, *pipeline)
-                    return f"Success! try #{n_clicks}", "alert alert-success"
+                    return f"Success!", "alert alert-success", children
                 except Exception as e:
-                    return f"Error: {e}", "alert alert-danger"
+                    return f"Error: {e}", "alert alert-danger", children
 
             else:
                 return (
                     "Please select columns and a clustering algorithm",
                     "alert alert-warning",
+                    children,
                 )
 
         else:
-            return "", ""
+            return "", "", children
 
     return html.Div(
         children=[
             html.H5("Excecute"),
-            html.Button(
-                "Run",
-                id=ids.RUN_BUTTON,
-                className="btn btn-primary",
-                style={"margin-right": "10px"},
+            html.Div(
+                id='dynamic-button-container',
+                children=[
+                    html.Button(
+                        "Run",
+                        id=ids.RUN_BUTTON,
+                        className="btn btn-primary",
+                        style={"margin-right": "10px", "width": "100px"},
+                    ),
+                ]
             ),
             html.H5(""),
             html.Div(id=ids.RESPONSE_TEXT_BUTTON, style={"margin-top": "5px"}),
