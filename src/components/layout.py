efbe43481872eb33b5948dd4cd737836863c5312
kpenaramirez <53351015+kpenaramirez@users.noreply.github.com>
2023-08-31 12:36:54 -0400

    Update layout.py

--- src/components/layout.py
+++ src/components/layout.py
@@ -19,8 +19,7 @@ def create_layout(app: Dash, source: DataSource) -> html.Div:
         className="app-div",
         children=[
             dcc.Markdown(f"# {app.title}", mathjax=True),
-            dcc.Markdown(f"Reanalize a couple of open clusters studied in Peña Ramírez et al. (2021, 2022) using the scikit-
-earn suite:"),
+            dcc.Markdown(f"Reanalize a couple of open clusters studied in Peña Ramírez et al. (2021, 2022) using the scikit_learn suite:"),
             html.Div(
                 [cluster_selection_dropdown.render(app, source)],
             ),
